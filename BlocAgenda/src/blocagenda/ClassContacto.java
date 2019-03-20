22ac263d952f13dac593dc22988f07c407121c35
brian <brian@DESKTOP-56I34FO>
2019-03-20 17:39:59 +0100

    ClassContacto

--- BlocAgenda/src/blocagenda/ClassContacto.java
+++ BlocAgenda/src/blocagenda/ClassContacto.java
@@ -1,11 +1,13 @@
 package blocagenda;
 
-/**
- *Exercici:
- *Descripció:
- * @author brian
- */
 import java.util.Scanner;
+
+/**
+ *Exercici: Agenda
+ *Descripció: class
+ * @author brian y ruben
+ */
+
 
 public class ClassContacto {
 
@@ -18,16 +20,124 @@ public class ClassContacto {
     private String direccion;
     
     
-    
+    public ClassContacto() {
+        nombre = " ";
+        primerApellido = " ";
+        segundoApellido = " ";
+        dni = 0;
+        edad = 0;
+        telefono = 0;
+        direccion = " ";
+    }
+
+    public ClassContacto(String nombre, String primerApellido, String segundoApellido,int dni, int edad, int telefono, String direccion) {
+        this.nombre = nombre;
+        this.primerApellido = primerApellido;
+        this.segundoApellido = segundoApellido;
+        this.dni = dni;
+        this.edad= edad;
+        this.telefono = telefono;
+        this.direccion=direccion;
+    }
     
 
-public ClassContacto() {
-    nombre = " ";
-    primerApellido = " ";
-    segundoApellido = " ";
-    dni = 0;
-    edad = 0;
-    telefono = 0;
-    direccion = " ";
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
     }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the primerApellido
+     */
+    public String getPrimerApellido() {
+        return primerApellido;
+    }
+
+    /**
+     * @param primerApellido the primerApellido to set
+     */
+    public void setPrimerApellido(String primerApellido) {
+        this.primerApellido = primerApellido;
+    }
+
+    /**
+     * @return the segundoApellido
+     */
+    public String getSegundoApellido() {
+        return segundoApellido;
+    }
+
+    /**
+     * @param segundoApellido the segundoApellido to set
+     */
+    public void setSegundoApellido(String segundoApellido) {
+        this.segundoApellido = segundoApellido;
+    }
+
+    /**
+     * @return the dni
+     */
+    public int getDni() {
+        return dni;
+    }
+
+    /**
+     * @param dni the dni to set
+     */
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    /**
+     * @return the edad
+     */
+    public int getEdad() {
+        return edad;
+    }
+
+    /**
+     * @param edad the edad to set
+     */
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    /**
+     * @return the telefono
+     */
+    public int getTelefono() {
+        return telefono;
+    }
+
+    /**
+     * @param telefono the telefono to set
+     */
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    /**
+     * @return the direccion
+     */
+    public String getDireccion() {
+        return direccion;
+    }
+
+    /**
+     * @param direccion the direccion to set
+     */
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+
 }
